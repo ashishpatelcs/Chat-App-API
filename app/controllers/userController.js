0c8af075760e015556f289b0899823a37fb3a6dd
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-12 11:31:00 +0530

    add signup method in usercontroller.js

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -13,9 +13,64 @@ const UserModel = mongoose.model('User')
 // start user signup function 
 
 let signUpFunction = (req, res) => {
+    let validateUserInput = () => {
+        return new Promise( (resolve, reject) => {
+            if(req.body.email) {
+                if(!validInput.Email(req.body.email)) {
+                    let apiResponse = response.generate(true, 'Email is Invalid', 500, null)
+                    reject(apiResponse)
+                } else if (check.isEmpty(req.body.password)) {
+                    let apiResponse = response.generate(true, 'Password is missing', 500)
+                    reject(apiResponse)
+                } else {
+                    resolve(req)
+                }
+            } else {
+                logger.error('email or password field missing', 'signUpFunction : validateUserInput', 10)
+                let apiResponse = response.generate(true, 'email or password field missing', 500, null)
+                reject(apiResponse)
+            }
+        })
+    }
 
-  
-
+    let createUser = () => {
+        return new Promise( (resolve, reject) => {
+            UserModel.findOne({ email: req.body.email })
+            .then(user => {
+                if(check.isEmpty(user)) {
+                    let newuser = new UserModel({
+                        userId: shortid.generate,
+                        firstName: req.body.firstName,
+                        lastName: req.body.lastName || '',
+                        email: req.body.email.toLowerCase(),
+                        mobileNumber: req.body.mobileNumber,
+                        password: passwordLib.hashpassword(req.body.password),
+                        apiKey: req.body.apiKey || req.params.apiKey || req.query.apiKey,
+                        createOn: time.now()
+                    })
+                    newuser.save( (err, user) => {
+                        if(err) {
+                            logger.error(err.message, 'signUpFunction: createUser', 10)
+                            let apiResponse = response.generate(true, 'Failed to create user', 500, null)
+                            reject(apiResponse)
+                        } else {
+                            let newuserObj = newuser.toObject()
+                            resolve(newuserObj)
+                        }
+                    })
+                } else {
+                    logger.error('cannot create user. already present', 'signUpFunction: createUser', 5)
+                    let apiResponse = response.generate(true, 'User already present with this email', 403, null)
+                    reject(apiResponse)
+                }
+            })
+            .catch(err => {
+                logger.error(err, 'SignUpFunction: createUser', 10)
+                let apiResponse = response.generate(true, 'Failed to create user', 500, null)
+                reject(apiResponse)
+            })
+        })
+    }
 }// end user signup function 
 
 // start of login function 
