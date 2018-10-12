5e7dd326bb611db43157c7accb39f4001528e0c6
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-12 09:32:37 +0530

    configure appconfig.js

--- config/appConfig.js
+++ config/appConfig.js
@@ -4,7 +4,7 @@ appConfig.port = 3000;
 appConfig.allowedCorsOrigin = "*";
 appConfig.env = "dev";
 appConfig.db = {
-    uri: 'mongodb://127.0.0.1:27017/myDB'
+    uri: 'mongodb://127.0.0.1:27017/chatAppDB'
   }
 appConfig.apiVersion = '/api/v1';
 
