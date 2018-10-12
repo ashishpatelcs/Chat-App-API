673fcdda2be0243017677811a6e31b05ae523f46
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-12 10:02:03 +0530

    add generateToken method to jwt token lib

--- app/libs/token.js
+++ app/libs/token.js
@@ -2,3 +2,30 @@ const jwt = require('jsonwebtoken')
 const shortid = require('shortid')
 
 const secretkey = 'somerandomstring'
+
+let generateToken = (data, cb) => {
+    try {
+        let currenttime = Date.now()
+        let claims = {
+            jwtid: shortid.generate(),
+            iat: currenttime,
+            exp: Math.floor(currenttime / 1000) + (60 * 60 * 24),
+            sub: 'authToken',
+            iss: 'edChat',
+            data
+        }
+        let tokenDetails = {
+            token: jwt.sign(claims, secretkey),
+            tokenSecret = secretkey
+        }
+        cb(null, tokenDetails)
+    }
+    catch(err) {
+        console.log(err)
+        cb(err, null)
+    }
+}
+
+module.exports = {
+    generateToken
+}
