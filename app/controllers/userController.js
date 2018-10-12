724a367714b9febbb052f6ab37a3dc1faf8c5722
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-12 09:31:56 +0530

    initial commit

---
+++ app/controllers/userController.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const shortid = require('shortid');
+const time = require('./../libs/timeLib');
+const response = require('./../libs/responseLib')
+const logger = require('./../libs/loggerLib');
+const validateInput = require('../libs/paramsValidationLib')
+const check = require('../libs/checkLib')
+
+/* Models */
+const UserModel = mongoose.model('User')
+
+
+// start user signup function 
+
+let signUpFunction = (req, res) => {
+
+  
+
+}// end user signup function 
+
+// start of login function 
+let loginFunction = (req, res) => {
+    
+}
+
+
+// end of the login function 
+
+
+let logout = (req, res) => {
+  
+} // end of the logout function.
+
+
+module.exports = {
+
+    signUpFunction: signUpFunction,
+    loginFunction: loginFunction,
+    logout: logout
+
+}// end exports
---
+++ app/models/User.js
@@ -0,0 +1,44 @@
+'use strict'
+/**
+ * Module Dependencies
+ */
+const mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+  userId: {
+    type: String,
+    default: '',
+    index: true,
+    unique: true
+  },
+  firstName: {
+    type: String,
+    default: ''
+  },
+  lastName: {
+    type: String,
+    default: ''
+  },
+  password: {
+    type: String,
+    default: 'passskdajakdjkadsj'
+  },
+  email: {
+    type: String,
+    default: ''
+  },
+  mobileNumber: {
+    type: Number,
+    default: 0
+  },
+  createdOn :{
+    type:Date,
+    default:""
+  }
+
+
+})
+
+
+mongoose.model('User', userSchema);
