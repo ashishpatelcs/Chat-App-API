e14971eb3f06deba50df86492f8667003dc6492c
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-15 08:57:01 +0530

    implemented Chat schema and  model

--- app/models/Chat.js
+++ app/models/Chat.js
@@ -2,12 +2,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 let chatSchema = new Schema({
+    chatId: { type: String, unique: true, required: true },
     createdOn: { type: Date, default: Date.now() },
+    modifiedOn: { type: Date, default: Date.now() },
     receiverId: { type: String, default: '' },
     receiverName: { type: String, default: '' },
     senderId: { type: String, default: '' },
     senderName: { type: String, default: '' },
-    message: { type: String, default: '' }
+    message: { type: String, default: '' },
+    seen: { type: Boolean, default: false },
+    chatRoom: { type: String, default: '' }
 })
 
 module.exports = mongoose.model('Chat', chatSchema)
